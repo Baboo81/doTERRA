8e242969de364901fdc578904dc2ec06b19bbd10
Baboo81 <magicbaboo@gmail.com>
2024-09-23 10:27:40 +0200

    Added class main

---
+++ src/be/babou/applicationdoterra/Main.java
@@ -0,0 +1,17 @@
+package be.babou.applicationdoterra;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int age = 17;
+		
+		System.out.println("Hello les amis"+ " " + "J'ai " + age);
+		
+		age = +1;
+		
+		System.out.println(age);
+
+	}
+
+}
